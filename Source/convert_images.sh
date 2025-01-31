86d66a14032a4a9144f923043c6c65a6c836e10e
Johannes Stegmaier <stegmaier@lfb.rwth-aachen.de>
2025-01-31 15:27:11 +0100

    changed shell file

--- Source/convert_images.sh
+++ Source/convert_images.sh
@@ -3,7 +3,10 @@
 input_file=$1
 output_path=$2
 
-MYCOMMAND='/netshares/scratch/stegmaier/Software/Environments/fusemycells/bin/python /netshares/scratch/stegmaier/Projects/2025/FuseMyCellsISBI_ImageFusion/Source/Source/convert_images.py '$input_file' '$output_path
+echo $input_file
+echo $output_path
+
+MYCOMMAND='/netshares/scratch/stegmaier/Software/Environments/fusemycells/bin/python /netshares/scratch/stegmaier/Projects/2025/FuseMyCellsISBI_ImageFusion/Source/Source/convert_images.py --input_file='$input_file' --output_path='$output_path
 
 echo $MYCOMMAND
 eval $MYCOMMAND
